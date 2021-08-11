8a1b65882b88d545be238303bbf4fe524e02e220
Soyboiii <ulriksin@gmail.com>
2021-08-11 22:24:51 +0200

    Added remove at index function and improved print explanations

--- src/usrArray/Main.java
+++ src/usrArray/Main.java
@@ -2,14 +2,23 @@ package usrArray;
 
 public class Main {
     public static void main(String[] args) {
+        //TODO make remove function
         ulsinArray uArr = new ulsinArray(5);
         uArr.insert(3);
         uArr.insert(5);
         uArr.insert(0);
         uArr.insert(7);
+
+        System.out.println("Printing the array after inserting 3 5 0 7");
         uArr.print();
 
+        System.out.println("Index of 7, then 3");
         System.out.println(uArr.indexOf(7));
         System.out.println(uArr.indexOf(3));
+
+        System.out.println("Print after removing the 4th entry, at the index of 3");
+        uArr.removeAt(3);
+        uArr.print();
+//        System.out.println(uArr.indexOf(5));
     }
 }
--- src/usrArray/ulsinArray.java
+++ src/usrArray/ulsinArray.java
@@ -21,6 +21,21 @@ public class ulsinArray {
         }
     }
 
+    public void removeAt(int index) {
+        // Search for index
+        // Make new array with every element except for the index
+        int[] tempArr = new int[intArr.length - 1];
+        int tempCounter = 0;
+        for (int i = 0; i < counter; i++) {
+            if (i != index) {
+                tempArr[tempCounter] = intArr[i];
+                tempCounter++;
+            }
+        }
+        intArr = tempArr;
+        counter = counter - 1;
+    }
+
     public int indexOf(int number) {
         int indexCounter = 0;
         for (int i = 0; i < counter; i++) {
